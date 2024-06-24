c1484f7623e69702e3dcd32ed06331ae989dbb62
Nishinoyama <begoingto1414@gmail.com>
2024-06-25 02:46:39 +0900

    Append dump in main.go

--- cmd/blinking-packet/main.go
+++ cmd/blinking-packet/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/until-tsukuba/blinking-packet/internal/api"
+	"github.com/until-tsukuba/blinking-packet/internal/dump"
 	"os"
 	"os/signal"
 	"syscall"
@@ -9,6 +10,7 @@ import (
 
 func main() {
 	api.Init()
+	go dump.DumpAndBlink("eth0", 60, 20)
 
 	trap := make(chan os.Signal, 1)
 	signal.Notify(trap, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGINT)
