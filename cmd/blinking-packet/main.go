326cc09ed9c525b0f8476291485b7170d090f526
Crow314 <me@crow31415.net>
2024-06-25 01:10:13 +0900

    Main function starts api server

--- cmd/blinking-packet/main.go
+++ cmd/blinking-packet/main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"github.com/until-tsukuba/blinking-packet/internal/api"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
+	api.Init()
+
+	trap := make(chan os.Signal, 1)
+	signal.Notify(trap, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGINT)
+	_ = <-trap
 }
