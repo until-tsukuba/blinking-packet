8ad9b3bd4ed210e85fb2eca1578d5346df0fcddb
Crow314 <me@crow31415.net>
2024-06-25 00:06:43 +0900

    Create server.go

---
+++ internal/api/server.go
@@ -0,0 +1,44 @@
+package api
+
+import (
+	"log"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+type server struct {
+	engine *gin.Engine
+}
+
+// Init initializes web server
+func Init() {
+	gin.SetMode("debug") // TODO: config
+
+	srv := newServer()
+	go srv.run()
+}
+
+// newServer generates web server
+func newServer() server {
+	srv := server{}
+
+	srv.engine = gin.Default()
+
+	// CORS setup
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowAllOrigins = true
+	srv.engine.Use(cors.New(corsConfig))
+
+	// srv.setRoutes()
+
+	return srv
+}
+
+// run starts web server
+func (srv *server) run() {
+	err := srv.engine.Run(":8080")
+	if err != nil {
+		log.Fatalf("Starting server was failed\n%v", err)
+	}
+}
